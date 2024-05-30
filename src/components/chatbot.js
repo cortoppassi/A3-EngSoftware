fc30646145e039c6060eed579912fc61e27e1779
cortoppassi <jonathan-516@hotmail.com>
2024-05-30 16:34:03 -0300

    fix/bot

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -9,7 +9,6 @@ import MicIcon from "@mui/icons-material/Mic";
 import CampaignIcon from "@mui/icons-material/Campaign";
 import MenuIcon from "@mui/icons-material/Menu";
 
-
 const chatBotStyle = {
   position: "fixed",
   bottom: "20px",
@@ -108,29 +107,25 @@ export default function ChatbotModal() {
   const handleOpen = () => {
     setOpen(true);
     setBotVisible(false);
-    setResposta(
-      <>
-        <h3>Olá! Como posso ajudar você hoje?</h3>
-      </>
-    );
+    addMessage({ role: "bot", content: "Olá! Como posso ajudar você hoje?" });
     if (textAreaRef.current) {
       textAreaRef.current.focus();
     }
   };
+
   const handleClose = () => {
     setOpen(false);
     setBotVisible(true);
-    setResposta("");
+    setMessages([]);
   };
+
   const [pergunta, setPergunta] = useState("");
-  const [resposta, setResposta] = useState("");
+  const [messages, setMessages] = useState([]);
   const [apiKey, setApiKey] = useState('');
   const [loading, setLoading] = useState(false);
   const [audioUrl, setAudioUrl] = useState("");
 
   const recognition = useRef(null);
-
-
 
   const startListening = () => {
     if (recognition.current) {
@@ -148,15 +143,17 @@ export default function ChatbotModal() {
     e.preventDefault();
     setLoading(true);
 
-    const userPrompt = "O projeto busca criar uma plataforma que reúna as principais ferramentas de engenharia de software para apoiar os desenvolvedores em todas as etapas do ciclo de vida do desenvolvimento. Como posso ajudar você hoje?";
+    const userPrompt = "Você vai responder de forma rapida clara e objetiva usando poucas palavras, assistente virtual especializado em engenharia de software, criado para ajudar desenvolvedores a encontrar as melhores ferramentas para cada etapa do seu projeto.";
 
     if (!apiKey) {
-      setResposta("Você não tem permissão");
+      addMessage({ role: "bot", content: "Você não tem permissão" });
       setLoading(false);
       return;
     }
 
-    setResposta("");
+    addMessage({ role: "user", content: pergunta });
+    setPergunta("");
+
     try {
       const resposta = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -183,8 +180,7 @@ export default function ChatbotModal() {
       );
 
       const respostaDoChat = resposta.data.choices[0].message.content;
-      // console.log("Resposta do Chat:", respostaDoChat);
-      setResposta(respostaDoChat);
+      addMessage({ role: "bot", content: respostaDoChat });
 
       const audioResposta = await axios.post(
         "https://api.openai.com/v1/audio/speech",
@@ -203,8 +199,6 @@ export default function ChatbotModal() {
         }
       );
 
-      // console.log("Resposta da API de Áudio:", audioResposta);
-
       if (audioResposta.headers["content-type"] === "audio/mpeg") {
         const blob = new Blob([audioResposta.data], { type: "audio/mpeg" });
         const audioUrl = URL.createObjectURL(blob);
@@ -219,7 +213,6 @@ export default function ChatbotModal() {
       console.error("Erro ao fazer pedido:", error.message);
     }
 
-    setPergunta("");
     setLoading(false);
   };
 
@@ -232,42 +225,31 @@ export default function ChatbotModal() {
   const handleOnChange = (e) => {
     setMenuVisible(false);
     setPergunta(e.target.value);
-    setRespostaOption("");
   };
 
-  const [respostaOption, setRespostaOption] = useState("");
+  const addMessage = (message) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  };
+
   const handleOptionClick = (option) => {
     switch (option) {
       case 1:
-        setRespostaOption(
-          <>
-            <h3>
-              Olá!<br></br> Eu sou o CodeHelper, o seu assistente virtual
-              especializado em engenharia de software. Fui criado para ajudar
-              desenvolvedores como você a encontrar as melhores ferramentas para
-              cada etapa do seu projeto.
-            </h3>
-          </>
-        );
-        toggleMenuVisibility();
+        addMessage({
+          role: "bot",
+          content: "Olá! Eu sou o CodeHelper, o seu assistente virtual especializado em engenharia de software. Fui criado para ajudar desenvolvedores como você a encontrar as melhores ferramentas para cada etapa do seu projeto.",
+        });
         break;
       case 2:
-        setRespostaOption(
-          <>
-            • Metodologia de Desenvolvimento: Conheça nossa abordagem para o
-            desenvolvimento de software e como entregamos produtos de qualidade.
-          </>
-        );
-        toggleMenuVisibility();
+        addMessage({
+          role: "bot",
+          content: "Metodologia de Desenvolvimento: Conheça nossa abordagem para o desenvolvimento de software e como entregamos produtos de qualidade.",
+        });
         break;
       case 3:
-        setRespostaOption(
-          <>
-            • Ferramentas Utilizadas: Descubra as tecnologias e ferramentas que
-            empregamos para criar soluções inovadoras.
-          </>
-        );
-        toggleMenuVisibility();
+        addMessage({
+          role: "bot",
+          content: "Ferramentas Utilizadas: Descubra as tecnologias e ferramentas que empregamos para criar soluções inovadoras.",
+        });
         break;
       case 4:
         const numeroWhatsapp = "71999214693";
@@ -276,18 +258,17 @@ export default function ChatbotModal() {
           mensagem
         )}`;
         window.open(linkWhatsapp, "_blank");
-        toggleMenuVisibility();
         break;
       case 5:
         window.open(
           "https://github.com/cortoppassi/A---3-Felipe/raw/main/A3%20Felipe%20Segunda%20Entrega.docx",
           "_blank"
         );
-        toggleMenuVisibility();
         break;
       default:
         console.log("Opção não reconhecida");
     }
+    toggleMenuVisibility();
   };
 
   const toggleMenuVisibility = () => {
@@ -344,7 +325,7 @@ export default function ChatbotModal() {
         }}
       >
         <Box sx={modalStyle}>
-        <div style={scrollableDivWithScrollbar}>
+          <div style={scrollableDivWithScrollbar}>
             <div>
               <MenuIcon
                 onClick={toggleMenuVisibility}
@@ -391,63 +372,24 @@ export default function ChatbotModal() {
               )}
             </div>
 
-            <div
-              style={{
-                overflow: "hidden",
-                display: "flex",
-                alignItems: "center",
-                backgroundColor: "#343541",
-              }}
-            >
-              {respostaOption && (
-                <p
-                  style={{
-                    color: "#bababa",
-                    display: pergunta ? "none" : "flex",
-                  }}
-                >
-                  {resposta ? "" : respostaOption}
-                </p>
-              )}
-              {resposta && (
-                <div>
-                  <h3
-                    style={{
-                      color: "#bababa",
-                      backgroundColor: "#1a1a1a",
-                      margin: "8px",
-                      padding: "8px",
-                      borderRadius: "8px",
-                      borderRadius: "0 8px 8px 8px",
-                      border: "solid 1px black",
-                    }}
-                  >
-                    {/* <button onClick={handlePlay}>
-                    <CampaignIcon></CampaignIcon>
-                  </button> */}
-                    {resposta}
-                  </h3>
-                </div>
-              )}
-            </div>
-
-            <div
-              style={{
-                overflow: "hidden",
-                backgroundColor: "#292929",
-                color: "#bababa",
-                margin: "8px",
-                padding: "8px",
-                borderRadius: "8px 0 8px 8px",
-                border: "solid 1px #ccc",
-                display: pergunta ? "block" : "none",
-                wordWrap: "break-word",
-              }}
-            >
-              <h3>{pergunta}</h3>
-            </div>
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                style={{
+                  overflow: "hidden",
+                  backgroundColor: message.role === "user" ? "#292929" : "#343541",
+                  color: "#bababa",
+                  margin: "8px",
+                  padding: "8px",
+                  borderRadius: "8px",
+                  border: "solid 1px #ccc",
+                  wordWrap: "break-word",
+                }}
+              >
+                <h3>{message.content}</h3>
+              </div>
+            ))}
           </div>
-          
           <div style={inputStyle}>
             <form
               onSubmit={handleSubmit}
